c21d70c1aa6b8f3143b29673895a4df7c59642ec
Kirill Mitskevich <k.mitskevich@datarockets.com>
2016-01-27 13:00:52 +0300

    Add communication_handler

---
+++ scripts/communication.js
@@ -0,0 +1,18 @@
+/*
+  Description:
+   Set array of answers for any question you want.
+   If somebody in chat write appropriate query, mona will answer him a random response for this query.
+*/
+
+var handlerCommunication = function(robot, queries, answers) {
+  queries.forEach(function(query) {
+    robot.hear(new RegExp(query, 'i'), function (response) {
+      response.send(response.random(answers))
+    });
+  })
+};
+
+module.exports = function (robot) {
+  handlerCommunication(robot, ['\\(╯°□°\）╯︵ ┻━┻'], ['┬─┬ノ( º _ ºノ) Calm down, bro.']);
+  handlerCommunication(robot, ['Доброе утро', 'Утро', 'Доброго утра', 'Good morning'], ['Доброе... :smiley', 'Привет!!! :smiley', 'И тебе! :smiley', 'Morning, bro :smiley'])
+};
