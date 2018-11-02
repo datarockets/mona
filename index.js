d017d649912bbdacb76e9a4cd8843679d0372dfb
viscoze <vlad.oganov@gmail.com>
2018-11-02 16:21:20 +0300

    Remove botkit studio code

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ if (!process.env.clientId || !process.env.clientSecret) {
   })
 
   const whereItsAt = 'https://' + process.env.PROJECT_DOMAIN + '.glitch.me/';
-  console.log('WARNING: This application is not fully configured to work with Slack. Please see instructions at ' + where_its_at);
+  console.log('WARNING: This application is not fully configured to work with Slack. Please see instructions at ' + whereItsAt);
 } else {
   webserver.get('/', function(req, res){
     res.render('index', {
@@ -126,34 +126,4 @@ if (!process.env.clientId || !process.env.clientSecret) {
   require("fs").readdirSync(normalizedPath).forEach(function(file) {
     require("./skills/" + file)(controller);
   });
-
-  // This captures and evaluates any message sent to the bot as a DM
-  // or sent to the bot in the form "@bot message" and passes it to
-  // Botkit Studio to evaluate for trigger words and patterns.
-  // If a trigger is matched, the conversation will automatically fire!
-  // You can tie into the execution of the script using the functions
-  // controller.studio.before, controller.studio.after and controller.studio.validate
-  if (process.env.studio_token) {
-    controller.on('direct_message,direct_mention,mention', function(bot, message) {
-      controller.studio.runTrigger(bot, message.text, message.user, message.channel, message).then(function(convo) {
-        if (!convo) {
-          // no trigger was matched
-          // If you want your bot to respond to every message,
-          // define a 'fallback' script in Botkit Studio
-          // and uncomment the line below.
-          // controller.studio.run(bot, 'fallback', message.user, message.channel);
-        } else {
-          // set variables here that are needed for EVERY script
-          // use controller.studio.before('script') to set variables specific to a script
-          convo.setVar('current_time', new Date());
-        }
-      }).catch(function(err) {
-        bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err);
-      });
-    });
-  } else {
-    console.log('~~~~~~~~~~');
-    console.log('NOTE: Botkit Studio functionality has not been enabled');
-    console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/');
-  }
 }
