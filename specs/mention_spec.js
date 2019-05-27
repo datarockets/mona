60f43cc2a5661e97fcc12a75a1f2197e666e6b7f
Nikita <n.sakov@datarockets.com>
2019-05-27 12:48:30 +0300

    Refactored spec structure in order to have one style everywhere

--- specs/mention_spec.js
+++ specs/mention_spec.js
@@ -1,24 +1,25 @@
 const assert = require('assert')
 const Botmock = require('botkit-mock')
-const mentionController = require('../features/mention.js')
-const responsesOnMention = require('../features/mention/replies')
 
-describe('Mentioning controller', () => {
+const mentionController = require('../features/mention.js')
+const replies = require('../features/mention/replies')
+
+describe('Mention controller', () => {
   beforeEach(() => {
     this.controller = Botmock({})
     this.bot = this.controller.spawn({ type: 'slack' })
     mentionController(this.controller)
   })
 
-  it('should return one of mention responds if user mentions bot', () => this.bot.usersInput([
-    {
-      type: 'direct_mention',
-      messages: [
-        {
-          text: 'bot',
-          isAssertion: true,
-        },
-      ],
+  it(
+    'should return one of mention responds if user mentions bot',
+    () => {
+      this.bot.usersInput([{
+        type: 'direct_mention',
+        messages: [{
+          text: 'bot', isAssertion: true,
+        }],
+      }]).then(message => assert(replies.includes(message.text)))
     },
-  ]).then(message => assert(responsesOnMention.includes(message.text))))
+  )
 })
