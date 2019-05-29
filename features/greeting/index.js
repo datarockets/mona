2f5e0bef882286b8cf172cd0b4c2cfaaa3b4b458
Nikita <n.sakov@datarockets.com>
2019-05-29 18:20:56 +0300

    Removed async for checking function because it works incorrectly with it

--- features/greeting/index.js
+++ features/greeting/index.js
@@ -6,7 +6,7 @@ const randomArrayItem = array => array[Math.floor(Math.random() * array.length)]
 
 module.exports = (controller) => {
   controller.hears(
-    async message => anyMatchesIgnoringCode(message, queries),
+    message => anyMatchesIgnoringCode(message, queries),
     ['message', 'direct_message'],
     async (bot, message) => {
       await bot.reply(message, { text: randomArrayItem(replies) })
