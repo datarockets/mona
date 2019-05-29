41673069fc26c269e5e426a42a651989fa88fd6c
Nikita <n.sakov@datarockets.com>
2019-05-29 11:04:39 +0300

    Implemented checking code function for greeting feature

--- features/greeting/index.js
+++ features/greeting/index.js
@@ -3,9 +3,15 @@ const replies = require('./replies.js')
 
 const randomArrayItem = array => array[Math.floor(Math.random() * array.length)]
 
+const anyMatchesIgnoringCode = (message, queries) => {
+  const messageWithoutCode = message.text.replace(/ *`[^)]*` */g, '')
+
+  return queries.map(query => messageWithoutCode.match(new RegExp(query, 'i'))).filter(Boolean).length > 0
+}
+
 module.exports = (controller) => {
   controller.hears(
-    queries,
+    async message => anyMatchesIgnoringCode(message, queries),
     ['message', 'direct_message'],
     async (bot, message) => {
       await bot.reply(message, { text: randomArrayItem(replies) })
