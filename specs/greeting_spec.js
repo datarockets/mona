8f6afd1a6e3d006e815e939526dba3a4d853032d
Nikita <n.sakov@datarockets.com>
2019-11-25 13:56:36 +0300

    Used new botkit-mock in specs

--- specs/greeting_spec.js
+++ specs/greeting_spec.js
@@ -1,40 +1,63 @@
+const { BotMock, SlackApiMock } = require('botkit-mock')
+const { SlackAdapter, SlackMessageTypeMiddleware, SlackEventMiddleware } = require('botbuilder-adapter-slack')
 const assert = require('assert')
-const Botmock = require('botkit-mock')
 
 const greetingController = require('../features/greeting')
 const replies = require('../features/greeting/replies.js')
+const { removeCodeFromMessage } = require('../lib/middleware')
 
 describe('Sample hears controller', () => {
   beforeEach(() => {
-    this.controller = Botmock({})
-    this.bot = this.controller.spawn({ type: 'slack' })
+    const adapter = new SlackAdapter({
+      clientSigningSecret: "secret",
+      botToken: "token",
+      debug: true
+    })
+
+    adapter.use(new SlackEventMiddleware())
+    adapter.use(new SlackMessageTypeMiddleware())
+
+    this.controller = new BotMock({
+      adapter: adapter,
+      disable_webserver: true
+    });
+
+    this.controller.middleware.ingest.use(removeCodeFromMessage)
+
+    SlackApiMock.bindMockApi(this.controller)
     greetingController(this.controller)
   })
 
   it(
-    'Should return any greeting if user types `hi`',
-    () => this.bot.usersInput([{
-      messages: [{
-        text: 'hi', isAssertion: true,
-      }],
-    }]).then(message => assert(replies.includes(message.text)))
-  )
+    'Should return any greeting if user types `hi`', async () => {
+      await this.controller.usersInput([{
+        type: 'message',
+        channel: 'channel',
+        messages: [{
+          text: 'hi', isAssertion: true,
+        }],
+      }]).then(message => assert(replies.includes(message.text)))
+  })
 
   it(
-    "Shouldn't return any greeting if user types hi in one line code block",
-    () =>this.bot.usersInput([{
-      messages: [{
-        text: '`hi`', isAssertion: true,
-      }],
-    }]).then(message => assert.deepEqual(message, {}))
-  )
+    "Shouldn't return any greeting if user types Morning in one line code block", async () => {
+      await this.controller.usersInput([{
+        type: 'message',
+        channel: 'channel',
+        messages: [{
+          text: '`Morning`', isAssertion: true,
+        }],
+      }]).then(message => assert.deepEqual(message, {}))
+  })
 
   it(
-    "Shouldn't return any greeting if user types hi in multiline code block",
-    () => this.bot.usersInput([{
-      messages: [{
-        text: '```hi```', isAssertion: true,
-      }],
-    }]).then(message => assert.deepEqual(message, {}))
-  )
+    "Shouldn't return any greeting if user types Morning in multiline code block", async () => {
+      await this.controller.usersInput([{
+        type: 'message',
+        channel: 'channel',
+        messages: [{
+          text: '```Morning```', isAssertion: true,
+        }],
+      }]).then(message => assert.deepEqual(message, {}))
+  })
 })
--- specs/mention_spec.js
+++ specs/mention_spec.js
@@ -1,23 +1,42 @@
+const { BotMock, SlackApiMock } = require('botkit-mock')
+const { SlackAdapter, SlackMessageTypeMiddleware, SlackEventMiddleware } = require('botbuilder-adapter-slack')
 const assert = require('assert')
-const Botmock = require('botkit-mock')
 
 const mentionController = require('../features/mention.js')
 const replies = require('../features/mention/replies')
+const adapter = require('../bot.js')
+const { removeCodeFromMessage } = require('../lib/middleware')
 
 describe('Mention controller', () => {
   beforeEach(() => {
-    this.controller = Botmock({})
-    this.bot = this.controller.spawn({ type: 'slack' })
+    const adapter = new SlackAdapter({
+      clientSigningSecret: "secret",
+      botToken: "token",
+      debug: true
+    });
+
+    adapter.use(new SlackEventMiddleware());
+    adapter.use(new SlackMessageTypeMiddleware());
+
+    this.controller = new BotMock({
+      adapter: adapter,
+      disable_webserver: true
+    });
+
+    this.controller.middleware.ingest.use(removeCodeFromMessage);
+
+    SlackApiMock.bindMockApi(this.controller)
     mentionController(this.controller)
   })
 
   it(
-    'should return one of mention responds if user mentions bot',
-    () => this.bot.usersInput([{
-      type: 'direct_mention',
-      messages: [{
-        text: 'bot', isAssertion: true,
-      }],
-    }]).then(message => assert(replies.includes(message.text)))
-  )
+    'should return one of mention responds if user mentions bot', async () => {
+      await this.controller.usersInput([{
+        type: 'direct_mention',
+        channel: 'channel',
+        messages: [{
+          text: 'bot', isAssertion: true,
+        }],
+      }]).then(message => assert(replies.includes(message.text)))
+  })
 })
