f25b09b63e7aa29268f60bb8e44a429694f10c06
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2019-12-11 22:59:12 +0500

    move adapter related code to separate file

---
+++ adapter.js
@@ -0,0 +1,55 @@
+require('dotenv').config()
+const { SlackAdapter, SlackMessageTypeMiddleware, SlackEventMiddleware } = require('botbuilder-adapter-slack')
+
+const tokenCache = JSON.parse(process.env.TOKENS || '{}')
+const userCache = JSON.parse(process.env.USERS || '{}')
+
+const getTokenForTeam = async (teamId) => {
+  if (tokenCache[teamId]) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(tokenCache[teamId])
+      }, 150)
+    })
+  }
+
+  return console.error('Team not found in tokenCache: ', teamId) // eslint-disable-line no-console
+}
+
+const getBotUserByTeam = async (teamId) => {
+  if (userCache[teamId]) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(userCache[teamId])
+      }, 150)
+    })
+  }
+
+  return console.error('Team not found in userCache: ', teamId) // eslint-disable-line no-console
+}
+
+const {
+  verificationToken,
+  clientSigningSecret,
+  botToken,
+  clientId,
+  clientSecret,
+  redirectUri,
+} = process.env
+
+const adapter = new SlackAdapter({
+  verificationToken,
+  clientSigningSecret,
+  botToken,
+  clientId,
+  clientSecret,
+  scopes: ['bot'],
+  redirectUri,
+  getTokenForTeam,
+  getBotUserByTeam,
+})
+
+adapter.use(new SlackEventMiddleware())
+adapter.use(new SlackMessageTypeMiddleware())
+
+module.exports = adapter
