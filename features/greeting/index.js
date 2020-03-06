e8b699545592a96694badf8bafee8ae4462ae66d
Nikita <n.sakov@datarockets.com>
2020-03-06 18:49:37 +0300

    Added check of greeting source. We do not reply on messages not from general, with mentions and from threads

--- features/greeting/index.js
+++ features/greeting/index.js
@@ -1,6 +1,21 @@
 const queries = require('./queries.js')
 const replies = require('./replies.js')
-const { randomArrayItem } = require('../../lib')
+const {
+  randomArrayItem,
+  messageFromChannels,
+  messageFromAnyThread,
+  messageHasMentions,
+} = require('../../lib')
+
+const shouldGreet = (message) => {
+  const notFromThread = !messageFromAnyThread(message)
+  const noMentions = !messageHasMentions(message)
+  const fromAllowableChannel = process.env.generalChannelId
+    ? messageFromChannels(message, [process.env.generalChannelId])
+    : true
+
+  return fromAllowableChannel && notFromThread && noMentions
+}
 
 module.exports = (controller) => {
   const matchKeys = queries.join('|')
@@ -10,7 +25,9 @@ module.exports = (controller) => {
     regexp,
     ['message', 'direct_message'],
     async (bot, message) => {
-      await bot.reply(message, { text: randomArrayItem(replies) })
+      if (shouldGreet(message)) {
+        await bot.reply(message, { text: randomArrayItem(replies) })
+      }
     },
   )
 }
