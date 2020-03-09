0ee3dfc9dade8ec7e254a049b66e2c698329a554
Nikita <n.sakov@datarockets.com>
2020-03-09 13:54:07 +0300

    Added tests for new cases in greeting feature

--- specs/greetingSpec.js
+++ specs/greetingSpec.js
@@ -13,7 +13,7 @@ describe('Sample hears controller', () => {
   it('returns any greeting if user types `hi`', async () => {
     await this.controller.usersInput([{
       type: 'message',
-      channel: 'channel',
+      channel: 'channelId',
       messages: [{
         text: 'hi', isAssertion: true,
       }],
@@ -23,7 +23,7 @@ describe('Sample hears controller', () => {
   it('does not return any greeting if user types `Morning` in one line code block', async () => {
     await this.controller.usersInput([{
       type: 'message',
-      channel: 'channel',
+      channel: 'channelId',
       messages: [{
         text: '`Morning`', isAssertion: true,
       }],
@@ -33,7 +33,7 @@ describe('Sample hears controller', () => {
   it('does not return any greeting if user types `Morning` in multiline code block', async () => {
     await this.controller.usersInput([{
       type: 'message',
-      channel: 'channel',
+      channel: 'channelId',
       messages: [{
         text: '```Morning```', isAssertion: true,
       }],
@@ -43,10 +43,62 @@ describe('Sample hears controller', () => {
   it('does not return any greeting if user types a word that includes greeting word', async () => {
     await this.controller.usersInput([{
       type: 'message',
-      channel: 'channel',
+      channel: 'channelId',
       messages: [{
         text: 'they', isAssertion: true,
       }],
     }]).then(message => assert.deepEqual(message, {}))
+  })
+
+  it('does not return any greeting if somebody mentioned', async () => {
+    await this.controller.usersInput([{
+      type: 'message',
+      channel: 'channelId',
+      messages: [{
+        text: '<@UJNAP9LQN>, hey', isAssertion: true,
+      }],
+    }]).then(message => assert.deepEqual(message, {}))
+  })
+
+  it('does not return any greeting in threads', async () => {
+    await this.controller.usersInput([{
+      type: 'message',
+      channel: 'channelId',
+      messages: [{
+        text: 'hey',
+        isAssertion: true,
+        thread_ts: '1583740417.000200',
+      }],
+    }]).then(message => assert.deepEqual(message, {}))
+  })
+
+  context('when generalChannelId specified', () => {
+    before(() => {
+      process.env.generalChannelId = 'generalId'
+    })
+
+    context('when in the general channel', () => {
+      it('returns any greeting if user types `hi`', async () => {
+        await this.controller.usersInput([{
+          type: 'message',
+          channel: 'generalId',
+          messages: [{
+            text: 'hi', isAssertion: true,
+          }],
+        }]).then(message => assert(replies.includes(message.text)))
+      })
+    })
+
+    context('when not in the general channel', () => {
+      it('does not return any greeting if user types `hi`', async () => {
+        await this.controller.usersInput([{
+          type: 'message',
+          channel: 'notGeneralId',
+          messages: [{
+            text: 'hi', isAssertion: true,
+          }],
+        }]).then(message => assert.deepEqual(message, {}))
+      })
+    })
   })
 })
